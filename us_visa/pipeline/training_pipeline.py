00c1792b06111dc6b20bcbad7ef13e5763717ec9
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-03-13 18:22:00 +0530

    Data transformation completed

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -2,10 +2,13 @@ import sys
 
 from us_visa.components.data_ingestion import DataINgestion
 from us_visa.components.data_validation import DataValidation
+from us_visa.components.data_transformation import DataTransformation
 from us_visa.entity.config_entity import (DataIngestionConfig,
-                                          DataValidationConfig)
+                                          DataValidationConfig,
+                                          DataTransformationConfig)
 from us_visa.entity.artifact_entity import (DataIngestionArtifact,
-                                            DataValidationArtifact)
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact)
 from us_visa.logger import logging
 from us_visa.exception import USvisaException
 
@@ -14,6 +17,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -56,6 +60,20 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys) from e
         
+    
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+        
 
         
     def run_pipeline(self) -> None:
@@ -65,6 +83,8 @@ class TrainPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
 
         except Exception as e:
             raise USvisaException(e, sys) from e
