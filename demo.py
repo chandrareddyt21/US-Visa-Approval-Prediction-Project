0e6276e42bc1b06d6bec8faaa47a0a78295329db
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-03-13 13:33:23 +0530

    checked ingestion pipeline

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
-
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.error(e)
-    raise USvisaException(e, sys) from e
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
