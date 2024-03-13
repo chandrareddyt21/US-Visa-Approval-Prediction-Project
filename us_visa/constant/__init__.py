be1c14e41038101f04f4d4c84a902e85f41549af
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-03-13 13:32:35 +0530

    constants added

--- us_visa/constant/__init__.py
+++ us_visa/constant/__init__.py
@@ -1,7 +1,7 @@
 import os
 from datetime import date
 
-DATABASE_NAME = "US_VISA"
+DATABASE_NAME = "us_visa"
 COLLECTION_NAME = "visa_data"
 MONGODB_URL_KEY = "MONGODB_URL"
 PIPELINE_NAME: str = "usvisa"
@@ -13,3 +13,12 @@ PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 FILE_NAME: str = "usvisa.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+
+"""
+Data INgestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: str = 0.2
