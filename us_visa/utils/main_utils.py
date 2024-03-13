2c6fe302780dd45a91691520d6ccf8932a064f2c
TIYYAGURA CHANDRA REDDY <chandra.tiyyagura@gmail.com>
2024-03-13 18:02:07 +0530

    data validation completd

--- us_visa/utils/main_utils.py
+++ us_visa/utils/main_utils.py
@@ -20,7 +20,7 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         if replace:
             if os.path.exists(file_path):
                 os.remove(file_path)
-        os.makedirs(os.path.dirname(file_path), exists_ok = True)
+        os.makedirs(os.path.dirname(file_path), exist_ok = True)
         with open(file_path, 'w') as file:
             yaml.dump(content, file)
     except Exception as e:
@@ -46,7 +46,7 @@ def save_numpy_array_data(file_path: str, array: np.array):
     """
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exists_ok = True)
+        os.makedirs(dir_path, exist_ok = True)
         with open(file_path, 'wb') as file_obj:
             np.save(file_obj, array)
     except Exception as e:
